5b779f673be62bc35dcf9d2f28ffb315d51b760d
Erhabor Ejiro David <daviderhabor.de@gmail.com>
2022-05-30 23:29:25 +0100

    fixed button and display functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const[ display, setDisplay] = useState('sound sound sound')
+  const handleClick = (x) => {
+    setDisplay(x)
+  }
+
+  
+
+  const [drums, setDrums] = useState([])
+  
+  
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/soundData");
+      const result = await res.json();
+      setDrums(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+
   return (
     <div className="App bg-blue-300 flex items-center justify-evenly m-5" id='drum-machine'>
-     
       <div className="buttons grid grid-cols-3 grid-rows-3 m-2 gap-4">
-        <button className='drum-pad' id='' >Q<audio src='' className='clip' id='Q'></audio></button>
-        <button className='drum-pad ' id='' >W<audio src='' className='clip' id='W'></audio></button>
-        <button className='drum-pad' id='' >E<audio src='' className='clip' id='E'></audio></button>
-        <button className='drum-pad' id='' >A<audio src='' className='clip' id='A'></audio></button>
-        <button className='drum-pad' id='' >S<audio src='' className='clip' id='S'></audio></button>
-        <button className='drum-pad' id='' >D<audio src='' className='clip' id='D'></audio></button>
-        <button className='drum-pad' id='' >Z<audio src='' className='clip' id='Z'></audio></button>
-        <button className='drum-pad' id='' >X<audio src='' className='clip' id='X'></audio></button>
-        <button className='drum-pad' id='' >C<audio src='' className='clip' id='C'></audio></button>
+        {drums.map((sd) => (
+          <button className='drum-pad' key={sd.keyCode} id={sd.id} onClick={() => handleClick(sd.id)} >{sd.keyTrigger}<audio src={sd.url} className='clip' id={sd.keyTrigger}></audio></button>
+        ))}
+       
       </div>
-      <div id='display' className=' bg-blue-100  w-2/6 m-2 p-4 text-center justify-items-center '>screen</div>
+      <div id='display' className=' bg-blue-100  w-2/6 m-2 p-4 text-center justify-items-center '>{display}</div>
     </div>
   );
 }
