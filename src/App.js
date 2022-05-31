2ffef7ee5e0844ec105bc4a66111532462bc84d0
Erhabor Ejiro David <daviderhabor.de@gmail.com>
2022-05-31 19:31:10 +0100

    made sound data locally accessible withing app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,62 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+
+var SoundData = [{
+    "keyCode": 81,
+    "keyTrigger": "Q",
+    "id": "Heater-2",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+  {
+   "keyCode": 87,
+    "keyTrigger": "W",
+    "id": "Heater-4-1",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" 
+  },
+  {
+   "keyCode": 69,
+    "keyTrigger": "E",
+    "id": "Dsc-Oh",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" 
+  },
+  {
+   "keyCode": 65,
+    "keyTrigger": "A",
+    "id": "RP4-KICK-1",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" 
+  },
+  {
+   "keyCode": 83,
+    "keyTrigger": "S",
+    "id": "Chord-1",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3" 
+  },
+  {
+   "keyCode": 68,
+    "keyTrigger": "D",
+    "id": "Chord-3",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3" 
+  },
+  {
+   "keyCode": 90,
+    "keyTrigger": "Z",
+    "id": "Dry-Ohh",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3" 
+  },
+  {
+   "keyCode": 88,
+    "keyTrigger": "X",
+    "id": "punchy-kick-1",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3" 
+  },
+  {
+   "keyCode": 67,
+    "keyTrigger": "C",
+    "id": "Brk-Snr",
+    "url": "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3" 
+  }]
+
+ 
+
 
 function App() {
 
@@ -25,24 +83,9 @@ function App() {
       } 
   }
 
-  //initialized all the data for the drum sounds url, keyCodes, sound names etc... to an empty array
-  const [drums, setDrums] = useState([])
+  //initialized all the data for the drum sounds url, keyCodes, sound names etc... to soundData
+  const [drums] = useState(SoundData)
 
-  //fetch updates the empty array with all the data retrieved from this json link, within app (http://localhost:8000/soundData)
-  const fetchData = async () => {
-    try {
-      const res = await fetch("http://localhost:8000/soundData");
-      const result = await res.json();
-      setDrums(result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  //useEffect ensures drums data is fetch on initial rendering/loading of page
-  useEffect(() => {
-    fetchData()
-  }, [])
 
   //React App ClassNames contain tailwindCss utility classes
   return (
